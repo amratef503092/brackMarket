d0332b01e1013a1737a2de3d8896f413618c4de2
Amr Atef Goda <41952049+amratef503092@users.noreply.github.com>
2024-05-08 22:09:15 +0300

    add

--- lib/view/news/presentation/view/news_screen.dart
+++ lib/view/news/presentation/view/news_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:renters/view/news/controller/cubit/news_cubit.dart';
 
+import '../../../../core/custom_widgets/custom_app_bar.dart';
+
 class NewsScreen extends StatelessWidget {
   const NewsScreen({super.key});
 
@@ -20,43 +22,44 @@ class NewsScreen extends StatelessWidget {
             return Center(child: Text('Error'));
           } else if (state is NewsLoaded) {
             return Scaffold(
+                appBar: appBarCustom(context),
                 body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: ListView.separated(
-                  itemCount: state.news.data!.length,
-                  separatorBuilder: (context, index) {
-                    return SizedBox(height: 10);
-                  },
-                  itemBuilder: (context, index) {
-                    return Container(
-                      padding: const EdgeInsets.all(8.0),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.grey),
-                        borderRadius: BorderRadius.circular(10),
-                      ),
-                      child: Column(
-                        children: [
-                          Container(
-                              height: 200,
-                              width: double.infinity,
-                              // color: Colors.grey,
-
-                              decoration: BoxDecoration(
-                                image: DecorationImage(
-                                  image: NetworkImage(
-                                      state.news.data![index].image!),
-                                  fit: BoxFit.cover,
-                                ),
-                              )),
-                          ListTile(
-                            title: Text(state.news.data![index].title!),
-                            // subtitle: Text('Subtitle'),
+                  padding: const EdgeInsets.all(8.0),
+                  child: ListView.separated(
+                      itemCount: state.news.data!.length,
+                      separatorBuilder: (context, index) {
+                        return const SizedBox(height: 10);
+                      },
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.all(8.0),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.grey),
+                            borderRadius: BorderRadius.circular(10),
                           ),
-                        ],
-                      ),
-                    );
-                  }),
-            ));
+                          child: Column(
+                            children: [
+                              Container(
+                                  height: 200,
+                                  width: double.infinity,
+                                  // color: Colors.grey,
+
+                                  decoration: BoxDecoration(
+                                    image: DecorationImage(
+                                      image: NetworkImage(
+                                          state.news.data![index].image!),
+                                      fit: BoxFit.cover,
+                                    ),
+                                  )),
+                              ListTile(
+                                title: Text(state.news.data![index].title!),
+                                // subtitle: Text('Subtitle'),
+                              ),
+                            ],
+                          ),
+                        );
+                      }),
+                ));
           } else {
             return Container();
           }
